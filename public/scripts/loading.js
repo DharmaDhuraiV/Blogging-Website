037f0106e7764dd98178261948b19cf05d0f3225
DharmaDhuraiV <ddhuraiv@gmail.com>
2021-11-12 00:49:58 +0530

    Files

--- public/scripts/loading.js
+++
@@ -1,9 +0,0 @@
-function redirect() {}
-
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
